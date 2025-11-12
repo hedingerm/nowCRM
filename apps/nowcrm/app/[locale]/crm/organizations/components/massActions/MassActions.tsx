5d7424a52103af75f0c4a9ca9a9d71767e2d986b
Nick <128822732+ToxicBlades@users.noreply.github.com>
2025-11-12 14:49:36 +0200

    fix: remove unused action add to list for orgs

--- apps/nowcrm/app/[locale]/crm/organizations/components/massActions/MassActions.tsx
+++ apps/nowcrm/app/[locale]/crm/organizations/components/massActions/MassActions.tsx
@@ -6,28 +6,10 @@ import {
 	type ActionsConfig,
 	massActionsGenerator,
 } from "@/components/generativeComponents/MassActionsGenerator";
-import AssignToListDialog from "./dialogs/addToList";
 import { MassDeleteOrganizations } from "./MassDeleteOrganizations";
-import { MassAddToList } from "./massAddToList";
 
 // Define the actions configuration for contacts
 const actionsConfig: ActionsConfig = {
-	assignToList: {
-		label: "Add to list", // e.g., "Assign to List"
-		requiresDialog: true,
-		dialogContent: ({ selectedOption, setSelectedOption }) => (
-			<AssignToListDialog
-				selectedOption={selectedOption}
-				setSelectedOption={setSelectedOption}
-			/>
-		),
-		dialogSubmitLabel: "Add to list",
-		onAction: async (selectedRows: DocumentId[], selectedOption: any) => {
-			return await MassAddToList(selectedRows, selectedOption.value);
-		},
-		successMessage: "Organizations added to list",
-		errorMessage: "Error during adding organizations to list",
-	},
 	deleteContacts: {
 		label: "Delete", // e.g., "Delete"
 		onAction: async (selectedRows: DocumentId[]) => {
